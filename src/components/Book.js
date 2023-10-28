57a3d0cb1e0aa313d1fcf27e34fa300636646dcc
Umu <hanny.ally123@gmail.com>
2023-10-28 21:47:22 +0300

    Fix eslint

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,9 +4,9 @@ const Book = ({
   id, title, author, category,
 }) => (
   <div className="book" key={id}>
-    <h3 className="category">{category}</h3>
+    <h3 className="category1">{category}</h3>
     <h2 className="booktitle">{title}</h2>
-    <h3 className="author">{author}</h3>
+    <h3 className="author1">{author}</h3>
     <button type="button" className="btn-remove">
       Remove
     </button>
