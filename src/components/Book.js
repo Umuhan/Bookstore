8b9f99175ce1ffeb58cecfe8e0154801b8617b87
Umu <hanny.ally123@gmail.com>
2023-11-01 20:53:42 +0300

    Update Book.j

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,20 +1,30 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
 const Book = ({
-  id, title, author, category,
-}) => (
-  <div className="book" key={id}>
-    <h3 className="category1">{category}</h3>
-    <h2 className="booktitle">{title}</h2>
-    <h3 className="author1">{author}</h3>
-    <button type="button" className="button-remove">
-      Remove
-    </button>
-  </div>
-);
+  itemId, title, author, category,
+}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="book" key={itemId}>
+      <h3 className="category1">{category}</h3>
+      <h2 className="booktitle">{title}</h2>
+      <h3 className="author1">{author}</h3>
+      <button
+        type="button"
+        className="button-remove"
+        onClick={() => dispatch(removeBook(itemId))}
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
 
 Book.propTypes = {
-  id: PropTypes.string.isRequired,
+  itemId: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
