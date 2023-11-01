986a85fcca51921c34e2e369218fb5609e5e0ac3
Umu <hanny.ally123@gmail.com>
2023-11-01 12:44:42 +0300

    Wrap provider component from react-redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Categories from './components/Categories';
-import Navbar from './components/Navbar';
-
-import BookList from './components/BookList';
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import BookList from "./components/BookList";
+import NewBooks from "./components/NewBooks";
 
 const App = () => (
-  <Router>
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<BookList />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="*" element={<h1>Error...</h1>} />
-      </Routes>
+  <Provider store={store}>
+    <div className="App">
+      <h1>Book List</h1>
+      <BookList />
+      <NewBooks />
     </div>
-  </Router>
+  </Provider>
 );
 
 export default App;
