855222963ccf510ebf56d80befc2c65a24109af7
Umu <hanny.ally123@gmail.com>
2023-11-01 12:45:28 +0300

    Modify Book.js file

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,11 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Book = ({
-  id, title, author, category,
-}) => (
+const Book = ({ id, title, author, category }) => (
   <div className="book" key={id}>
     <h3 className="category1">{category}</h3>
     <h2 className="booktitle">{title}</h2>
     <h3 className="author1">{author}</h3>
-    <button type="button" className="btn-remove">
+    <button type="button" className="button-remove">
       Remove
     </button>
   </div>
