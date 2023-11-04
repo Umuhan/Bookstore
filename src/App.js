11e57a8d6479d83888476c4180d5e4ea42db616e
Umu <hanny.ally123@gmail.com>
2023-11-04 18:08:58 +0300

    Remove book list to books

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import BookList from './components/BookList';
-import NewBooks from './components/NewBooks';
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import BookList from "./components/BookList";
 
 const App = () => (
   <Provider store={store}>
     <div className="App">
-      <h1>Book List</h1>
+      <h1 className="book book-app">Books</h1>
       <BookList />
-      <NewBooks />
     </div>
   </Provider>
 );
