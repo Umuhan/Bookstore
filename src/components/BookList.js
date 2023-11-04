6a4a58a4b5f4c961cb4c1bbcfb9a1e38f0ce278f
Umu <hanny.ally123@gmail.com>
2023-11-04 18:11:29 +0300

    Add Book AddNew and fetchBook to the bookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Book from './Book';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import AddNew from "./NewBooks";
+import Book from "./Book";
+import { fetchBooks } from "../redux/books/booksSlice";
 
 const BookList = () => {
-  const books = useSelector((state) => state.books.books);
+  const { books } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
 
   return (
-    <>
-      <div className="book-list">
+    <div className="main">
+      <div className="">
         {books.map((book) => (
           <Book
-            key={book.itemId}
-            itemId={book.itemId}
+            key={book.item_id}
+            item_id={book.item_id}
             title={book.title}
             author={book.author}
             category={book.category}
           />
         ))}
       </div>
-    </>
+      <AddNew />
+    </div>
   );
 };
 
